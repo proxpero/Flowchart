74f3dd2c6cdae1e7a78b5cbea37e7abf6ea968dd
Todd Olsen <todd.olsen@me.com>
2016-05-10 10:51:21 -0400

    Views and buttons

---
+++ Flowchart/Flowchart/FlowChartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlowChartViewController.swift
+//  Flowchart
+//
+//  Created by Todd Olsen on 5/10/16.
+//  Copyright © 2016 Todd Olsen. All rights reserved.
+//
+
+import UIKit
+
+class FlowChartViewController: UIViewController {
+
+    @IBOutlet weak var libraryView: UIStackView!
+
+
+
+
+
+
+
+
+    @IBAction func toggleLibraryAction(sender: UIButton) {
+
+        UIView.animateWithDuration(0.3) {
+            self.libraryView.hidden = !self.libraryView.hidden
+        }
+
+    }
+    
+}
