638e01dbbe4587fae5ad8ea06a16bde49c88d7c1
Todd Olsen <todd.olsen@me.com>
2016-05-11 16:09:00 -0400

    For ui purposes, embed UICollectionView in UIViewController, rather than have a dedicated UICollectionViewController

--- Flowchart/Flowchart/DecisionLibraryViewController.swift
+++ Flowchart/Flowchart/DecisionLibraryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DecisionLibraryViewController: UICollectionViewController {
+class DecisionLibraryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var decisions: [Decision] = [
         Decision.IsEven,
@@ -16,24 +16,31 @@ class DecisionLibraryViewController: UICollectionViewController {
         Decision.IsLessThan(0),
         Decision.IsGreaterThan(0)
     ]
-
+    
     var didSelectDecision: (Decision) -> () = { _ in }
 
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return decisions.count
     }
 
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DecisionCell", forIndexPath: indexPath) as? DecisionCell else { fatalError() }
-        cell.layer.borderColor = UIColor.Steel.CGColor
+        cell.layer.borderColor = UIColor.whiteColor().CGColor
         cell.layer.borderWidth = 1.0
         cell.layer.cornerRadius = 5.0
         cell.backgroundColor = UIColor.Mercury
         return decisions[indexPath.row].configure(cell)
     }
 
-    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         didSelectDecision(decisions[indexPath.row])
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.layer.cornerRadius = 5.0
+        view.layer.borderColor = UIColor.Mercury.CGColor
+        view.layer.borderWidth = 1.0
     }
 
 }
@@ -46,7 +53,7 @@ class DecisionCell: UICollectionViewCell {
 extension Decision {
 
     func configure(cell: DecisionCell) -> DecisionCell {
-        cell.label.text = self.title.stringByReplacingOccurrencesOfString(" {Input} ", withString: " ")
+        cell.label.text = self.title
         return cell
     }
 
